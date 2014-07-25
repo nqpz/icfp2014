181f6e6cdab3013cf6c5164ba1b79c39437efda7
Oleksandr Shturmov <oleks@oleks.info>
2014-07-25 19:21:16 +0200

    fixups

--- Ghc.hs
+++ Ghc.hs
@@ -1,20 +1,21 @@
-data Arg = Const Int | Reg String | Mem String
-data Instr = Mov Reg Reg
-           | INC Reg
-           | DEC Reg
-           | ADD Reg Reg
-           | SUB Reg Reg
-           | MUL Reg Reg
-           | DIV Reg Reg
-           | AND Reg Reg
-           | OR  Reg Reg
-           | XOR Reg Reg
-           | JLT Reg Reg
-           | JEQ Reg Reg
-           | JGT Reg Reg
+import Data.Word(Word8)
+
+data Arg = Reg Char | IndReg Char | Const Word8 | Mem Word8
+data Instr = MOV Arg
+           | INC Arg
+           | DEC Arg
+           | ADD Arg Arg
+           | SUB Arg Arg
+           | MUL Arg Arg
+           | DIV Arg Arg
+           | AND Arg Arg
+           | OR  Arg Arg
+           | XOR Arg Arg
+           | JLT Arg Arg
+           | JEQ Arg Arg
+           | JGT Arg Arg
            | INT Int
            | HLT
-
 
 -- Data flow analysis:
 -- Init: Mem = [0, 0, ... , 0], a = b = 0.
