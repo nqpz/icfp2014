e3356772425f2df1b1daffdeaa459b2c75428eb5
Mathias Svensson <idolf@pwnies.dk>
2014-07-25 19:07:11 +0200

    Bedre mellemkode

--- SuperLambda.hs
+++ SuperLambda.hs
@@ -13,20 +13,15 @@ data BinOp = Add
            | Gte
 
 data Expr = IntVal LI.Number -- new int
-          | ArrayVal Int -- new array with a length
-          | Index Int Expr -- take from array
           | BinOp BinOp Expr Expr -- binary op on ints
           | Var Name -- a variable
-          | Cons Expr Expr -- create tuple
-          | Car Expr -- get from tuple
-          | Cdr Expr -- get second from tuple
-          | FunName Name -- function pointer
-          | Popped -- take from data stack
+          | Cons Expr Expr -- (a, b)
+          | Car Expr -- let (a, _) in a // fst
+          | Cdr Expr -- let (_, b) in b // snd
           | IfThenElse Expr Expr Expr -- if x then y else z
           | Let Name Expr Expr -- let x = y in z
           | CallFun Name [Var] -- f(a, b, ...)
-          | ArraySet Arg Int Expr Expr -- arg[i] = x in y
-          | LowLevel LI.Instr Expr -- <low level instruction> in x
+          | LowLevel LI.Instr -- <low level instruction> 
 
 data Fun = FunDef Name [Var] Expr
 
