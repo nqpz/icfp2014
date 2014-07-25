0c3e17d7dec87f7ef24be189378480e8c86265c8
Kristoffer Søholm <k.soeholm@gmail.com>
2014-07-25 16:38:24 +0200

    Ghost datatypes

---
+++ Ghc.hs
@@ -0,0 +1,17 @@
+
+data Arg = Const Int | Reg String | Mem String
+data Instr = Mov Reg Reg
+           | INC Reg
+           | DEC Reg
+           | ADD Reg Reg
+           | SUB Reg Reg
+           | MUL Reg Reg
+           | DIV Reg Reg
+           | AND Reg Reg
+           | OR  Reg Reg
+           | XOR Reg Reg
+           | JLT Reg Reg
+           | JEQ Reg Reg
+           | JGT Reg Reg
+           | INT Int
+           | HLT
