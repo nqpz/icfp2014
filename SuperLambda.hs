4e10927eef96dc3635d99102c3d5778ef9f8922e
Niels G. W. Serup <ngws@metanohi.name>
2014-07-25 18:31:56 +0200

    fix high-level language description

--- SuperLambda.hs
+++ SuperLambda.hs
@@ -1,6 +1,6 @@
 -- hybrid between low-level and high-level
 
-import qualified LambdaMan as LM
+import qualified LambdaInstrs as LI
 
 newtype Name = Name String
 newtype Arg = Arg String
@@ -9,8 +9,11 @@ data BinOp = Add
            | Sub
            | Mul
            | Div
+           | Eq
+           | Gt
+           | Gte
 
-data Expr = IntVal LM.Number -- new int
+data Expr = IntVal LI.Number -- new int
           | ArrayVal Int -- new array with a length
           | Index Int Expr -- take from array
           | BinOp BinOp Expr Expr -- binary op on ints
@@ -21,7 +24,7 @@ data Expr = IntVal LM.Number -- new int
           | FunName Name -- function pointer
           | Popped -- take from data stack
 
-data Instr = LMInstr LM.Instr -- base instruction
+data Instr = LIInstr LI.Instr -- base instruction
            | IfThenElse Expr [Instr] [Instr] -- conditional
            | Let Arg Expr -- make a variable and set its value
            | FunDef Name [Arg] [Instr] -- a function
